ced0eedd0533ad41e3470ab236bd0a3dad5931c4
IPONOMAR <ihor.ponomarov@kb.cz>
2026-02-02 11:12:45 +0100

    Typo fixed in registrationData, steps added to the test.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -16,43 +16,55 @@ test.afterEach("Close the page", async ({ context }) => {
 test("Login with email contains public domain @Login @Negative", async ({
   page,
 }) => {
-  const homePage = new HomePage(page);
-  await homePage.navigate();
-  await homePage.validateAllComponents();
-
-  const cookieBar = new CookieBar(page);
-  await cookieBar.validateAllComponents();
-  await cookieBar.acceptCookies();
-
-  const mainMenu = new MainMenuDesktop(page);
-  await mainMenu.validateAllComponents();
-  await mainMenu.hoverOnSupport();
-
-  const mainMenuSupport = new MainMenuSupport(page);
-  await mainMenuSupport.validateAllComponents();
-  await mainMenuSupport.rdForumsClick();
-
-  const rdForums = new RdForums(page);
-  await rdForums.validateAllComponents();
-  await rdForums.registerClick();
-
-  const registrationTerms = new RegistrationTerms(page);
-  await registrationTerms.validateAllComponents();
-  await registrationTerms.agreeWithTerms();
-
   const registrationForm = new RegistrationForm(page);
-  await registrationForm.validateAllComponents();
-  await registrationForm.fillRegistrationForm(
-    testData[0].userName,
-    testData[0].userPassword,
-    testData[0].userPassword,
-    testData[0].userEmail,
-    testData[0].userFulllName,
-    testData[0].timeZone
-  );
-  await registrationForm.submitRegistrationForm();
 
-  const registrationError = new RegistrationErrorPage(page);
-  await registrationError.validateAllComponents();
-  await registrationError.validateErrorMessageText();
+  await test.step("Navigate to Veeam Home Page", async () => {
+    const homePage = new HomePage(page);
+    await homePage.navigate();
+    await homePage.validateAllComponents();
+  });
+
+  await test.step("Accept cookies to continue", async () => {
+    const cookieBar = new CookieBar(page);
+    await cookieBar.validateAllComponents();
+    await cookieBar.acceptCookies();
+  });
+
+  await test.step("Hover on Support section of the main menu and navigate to R&D Forums", async () => {
+    const mainMenu = new MainMenuDesktop(page);
+    await mainMenu.validateAllComponents();
+    await mainMenu.hoverOnSupport();
+    const mainMenuSupport = new MainMenuSupport(page);
+    await mainMenuSupport.validateAllComponents();
+    await mainMenuSupport.rdForumsClick();
+  });
+
+  await test.step("Validate R&D Forums components and start registration process", async () => {
+    const rdForums = new RdForums(page);
+    await rdForums.validateAllComponents();
+    await rdForums.registerClick();
+  });
+
+  await test.step("Validate Registration Terms, agree with terms and fill registration form", async () => {
+    const registrationTerms = new RegistrationTerms(page);
+    await registrationTerms.validateAllComponents();
+    await registrationTerms.agreeWithTerms();
+
+    await registrationForm.validateAllComponents();
+    await registrationForm.fillRegistrationForm(
+      testData[0].userName,
+      testData[0].userPassword,
+      testData[0].userPassword,
+      testData[0].userEmail,
+      testData[0].userFullName,
+      testData[0].timeZone,
+    );
+  });
+
+  await test.step("Submit registration form and validate error message for public domain email", async () => {
+    await registrationForm.submitRegistrationForm();
+    const registrationError = new RegistrationErrorPage(page);
+    await registrationError.validateAllComponents();
+    await registrationError.validateErrorMessageText();
+  });
 });
